5c755ed1270b995fde97584b9a40569147eb746c
Elessandra Estevão <elessandraestevao@hotmail.com>
2014-06-15 18:34:42 -0300

    Pacote Model inicial.

---
+++ src/java/br/com/buskimoveis/model/entity/Imobiliaria.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package br.com.buskimoveis.model.entity;
+
+/**
+ *
+ * @author Elessandra
+ */
+public class Imobiliaria {
+    Long id;
+    String razaoSocial;
+    String nomeFantasia;
+    String cnpj;
+    String creci;
+    String rua;    
+}
